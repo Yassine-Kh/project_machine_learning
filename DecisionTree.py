7f5cfa5408ea71dc4e28354ed73768d5390dc5af
unknown <aladinnweli@gmail.com>
2021-12-06 02:42:27 +0100

    Add boxplot

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,3 +1,16 @@
+#@author : Ala Eddine NOUALI
+
+import pandas as pd
+from sklearn.model_selection import train_test_split, ShuffleSplit, cross_val_score
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from pandas.api.types import is_numeric_dtype
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class DecisionTree:
 
     def __init__(self, n_depths=10):
@@ -5,19 +18,19 @@ class DecisionTree:
         self.n_depths = n_depths
         self.depths = np.linspace(1, 10, self.n_depths)
 
-    def classification(self):
-        tab_RMSE_tree = np.zeros(self.n_depths)
+    def classification(self,  X_train, y_train):
+        tab_log_loss_tree = np.zeros(self.n_depths)
+        tab_log_loss_tree_box = []
         for i in range(self.n_depths):
             reg_tree = DecisionTreeClassifier(max_depth=self.depths[i])
-            tab_RMSE_tree[i] = np.median(
-                np.sqrt(-cross_val_score(reg_tree, X_train, y_train, scoring='neg_log_loss', cv=self.cvp)))
-        return tab_RMSE_tree
+            log_loss = np.sqrt(-cross_val_score(reg_tree, X_train, y_train, scoring='neg_log_loss', cv=self.cvp))
+            tab_log_loss_tree_box.append(log_loss)
+            tab_log_loss_tree[i] = np.median(log_loss)
+        return tab_log_loss_tree, tab_log_loss_tree_box
 
-    def plot(self):
-        plt.plot(self.depths, self.classification())
-        plt.axis([0, 6, 0, 20])
+    def plot(self, X_train, y_train):
+        plt.plot(self.depths, self.classification(X_train, y_train)[0])
+        plt.boxplot(self.classification(X_train, y_train)[1])
+        plt.xlabel('Max depth of the tree', size=20)
+        plt.ylabel('CROSS-Validation', size=20)
         plt.show()
-
-reg_tree = DecisionTree()
-reg_tree.plot()
-plt.show()
