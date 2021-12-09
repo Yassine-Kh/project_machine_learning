e2383e78347cb284f5461a065436167e5280cb89
Yassine-Kh <56275009+Yassine-Kh@users.noreply.github.com>
2021-12-09 19:34:54 +0100

    Testing get_columns and set_columns from cleanData

--- tests/test_utils_pytest.py
+++ tests/test_utils_pytest.py
@@ -1,50 +1,36 @@
 import pytest
 import pandas as pd
-
+from math import *
 from utils import CleanData
 
 
 @pytest.fixture
 def clean_data_class_banknote():
     file_path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt"
-    clean_data_obj = CleanData(file_path)
+    data_to_clean = CleanData(file_path)
     real_df = pd.read_csv(file_path)
-    return [clean_data_obj, real_df](request.param)
+    data = [data_to_clean, real_df]
+    return data
 
 
-
-@pytest.fixture
+@pytest.fixture()
 def clean_data_class_kidney():
     file_path = "..\kidney_disease.csv"
-    clean_data_obj = CleanData(file_path)
+    data_to_clean = CleanData(file_path)
     real_df = pd.read_csv(file_path)
-    return [clean_data_obj, real_df]
+    return [data_to_clean, real_df]
 
 
-@pytest.mark.parametrize("clean_data_obj", "real_df", [
-    (clean_data_class_kidney[0], clean_data_class_kidney[1]),
-    (clean_data_class_banknote[0], clean_data_class_banknote[1]),
-    indirect=True])
-
-def test_get_columns(clean_data_obj, real_df):
-    assert (clean_data_obj.getColumns()[k] == real_df.columns()[k] for k in range(len(real_df.columns)))
+@pytest.mark.parametrize("data", ["clean_data_class_banknote", "clean_data_class_kidney"])
+def test_get_columns(data, request):
+    data = request.getfixturevalue(data)
+    data_to_clean, real_df = data[0], data[1]
+    assert (data_to_clean.getColumns()[k] == real_df.columns[k] for k in range(len(real_df.columns)))
 
 
-def test_set_columns():
-    assert False
+def test_set_columns(clean_data_class_banknote):
+    data_to_clean, real_df = clean_data_class_banknote[0], clean_data_class_banknote[1]
+    column_names = ["variance", "skewness", "curtosis", "entropy", "classification"]
+    data_to_clean.setColumns(column_names)
+    assert (data_to_clean.getColumns()[k] == column_names[k] for k in range(len(column_names)))
 
-
-def test_check_data():
-    assert False
-
-
-def test_clean_data():
-    assert False
-
-
-def test_scale_data():
-    assert False
-
-
-def test_split_data():
-    assert False
