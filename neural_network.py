84f016a950c100b6d3bcb2dde6ea8249ce99c2f4
achraftha <thabet.achraf@yahoo.com>
2021-12-09 01:21:00 +0100

    Adding the description of different parameters to test

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,4 @@
+from utils import *
 
 class NeuralNetwork():
     def __init__(self, solver="lbfgs" , layers_sizes=(5,2), activation="logistic", learning_rate=0.001):
@@ -21,5 +22,18 @@ class NeuralNetwork():
         """
         self.model.fit(X_train, y_train)
         return self.model.score(X_test, y_test)
-  
-print("test")     
+
+
+data1 = CleanData("https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt")
+column_names = ["variance", "skewness", "curtosis", "entropy", "classification"]
+X_train, X_test, y_train, y_test = data1.splitData(1/3, 42, column_names)
+
+
+modelNeural=NeuralNetwork(solver="lbfgs" , layers_sizes=(5,2), activation="logistic", learning_rate=0.001)
+
+# solver=["lbfgs", "adam", "sgd"] even though lbgs is used in classification
+# layers_sizes= choose different number of layers and different number of neurons per layer (last layer has to be 2)
+# activation=["logistic", "tanh"]
+# learning_rate=[range(0.001,0.1,10)]
+
+modelNeural.fitAndScore(X_train, X_test, y_train, y_test)
