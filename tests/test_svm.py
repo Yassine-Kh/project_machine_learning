e330c106a61bf6b233aab42edd9b27cf2127ac0b
Yassine-Kh <56275009+Yassine-Kh@users.noreply.github.com>
2021-12-15 19:49:49 +0100

    Updating svm tests

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -1,7 +1,8 @@
+import numpy
 import pytest
 import pandas as pd
 
-from models.svm import SVM
+from models.svm import SVM, bestParameters
 from utils import CleanData
 
 """
@@ -49,3 +50,15 @@ def test_fitAndPredict(data, request):
     assert model_svm.assertEqual(str(model_svm), 'Please train your model first')
     model_svm.fitAndPredict(X_train, X_test, y_train)
     assert not model_svm.assertEqual(str(model_svm), 'Please train your model first')
+
+
+@pytest.mark.parametrize("data", ["clean_data_class_banknote", "clean_data_class_kidney"])
+def test_bestParameters(data, request):
+    data = request.getfixturevalue(data)
+    data_to_clean, real_df = data[0], data[1]
+    column_names = ["variance", "skewness", "curtosis", "entropy", "classification"]
+    X_train, X_test, y_train, y_test = data_to_clean.splitData(1 / 3, 42, column_names)
+    kernel = ["linear", "poly", "rbf", "sigmoid"]
+    gamma = ["scale", "auto"]
+    tol = numpy.linspace(0.001, 0.1, 10)
+    assert type(bestParameters(X_train, X_test, y_train, y_test, kernel, gamma, tol)) == str
